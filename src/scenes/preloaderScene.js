1df2f1656df17dd4efe8b978285bdb4904136cad
George <71434803+george-swift@users.noreply.github.com>
2021-06-09 23:09:05 +0100

    Update publish directory and paths

--- src/scenes/preloaderScene.js
+++ src/scenes/preloaderScene.js
@@ -74,18 +74,18 @@ export default class PreloaderScene extends Phaser.Scene {
 
     this.load.audio('bgMusic', ['assets/audio/LivelyMeadow.mp3']);
 
-    this.load.image('blueButton1', 'assets/ui/blue_button02.png');
-    this.load.image('blueButton2', 'assets/ui/blue_button03.png');
-    this.load.image('greyBox', 'assets/ui/grey_box.png');
-    this.load.image('checkedBox', 'assets/ui/blue_boxCheckmark.png');
-    this.load.image('background', 'assets/images/background.png');
-    this.load.image('star', 'assets/images/star.png');
-    this.load.image('ground', 'assets/images/tile.png');
-    this.load.image('higherGround', 'assets/images/tile-2.png');
-    this.load.image('higherGroundLeft', 'assets/images/tile-3.png');
-    this.load.image('bomb', 'assets/images/bomb.png');
+    this.load.image('blueButton1', './assets/ui/blue_button02.png');
+    this.load.image('blueButton2', './assets/ui/blue_button03.png');
+    this.load.image('greyBox', './assets/ui/grey_box.png');
+    this.load.image('checkedBox', './assets/ui/blue_boxCheckmark.png');
+    this.load.image('background', './assets/images/background.png');
+    this.load.image('star', './assets/images/star.png');
+    this.load.image('ground', './assets/images/tile.png');
+    this.load.image('higherGround', './assets/images/tile-2.png');
+    this.load.image('higherGroundLeft', './assets/images/tile-3.png');
+    this.load.image('bomb', './assets/images/bomb.png');
 
-    this.load.spritesheet('ninja', 'assets/images/ninja.png', { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('ninja', './assets/images/ninja.png', { frameWidth: 32, frameHeight: 48 });
 
     this.load.plugin('rexgridtableplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexgridtableplugin.min.js', true);
   }
